61f07a003c2f31982060d0fafaf3736a018c2a57
Fuzzbawls <fuzzbawls@gmail.com>
2016-02-03 18:16:23 -0800

    Update Qt tests

--- src/qt/test/uritests.cpp
+++ src/qt/test/uritests.cpp
@@ -1,17 +1,9 @@
 #include "uritests.h"
-#include "../guiutil.h"
-#include "../walletmodel.h"
+
+#include "guiutil.h"
+#include "walletmodel.h"
 
 #include <QUrl>
-
-/*
-struct SendCoinsRecipient
-{
-    QString address;
-    QString label;
-    qint64 amount;
-};
-*/
 
 void URITests::uriTests()
 {
@@ -59,9 +51,8 @@ void URITests::uriTests()
     QVERIFY(rv.address == QString("175tWpb8K1S7NmH4Zx6rewF9WQrcZv245W"));
     QVERIFY(rv.label == QString());
 
-    // We currently don't implement the message parameter (ok, yea, we break spec...)
     uri.setUrl(QString("bitcoin:175tWpb8K1S7NmH4Zx6rewF9WQrcZv245W?req-message=Wikipedia Example Address"));
-    QVERIFY(!GUIUtil::parseBitcoinURI(uri, &rv));
+    QVERIFY(GUIUtil::parseBitcoinURI(uri, &rv));
 
     uri.setUrl(QString("bitcoin:175tWpb8K1S7NmH4Zx6rewF9WQrcZv245W?amount=1,000&label=Wikipedia Example"));
     QVERIFY(!GUIUtil::parseBitcoinURI(uri, &rv));
