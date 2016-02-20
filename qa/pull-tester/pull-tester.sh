4b11441c6494f42801da60b9a831d145d75f7722
Fuzzbawls <fuzzbawls@gmail.com>
2016-02-19 20:58:25 -0800

    Back port Tests from 0.12

--- qa/pull-tester/pull-tester.sh
+++ qa/pull-tester/pull-tester.sh
@@ -1,15 +1,2 @@
-#!/bin/sh
-# Helper script for pull-tester.
-#Param 1: path to bitcoin srcroot
-#Param ...: arguments for build-test.sh
-
-if [ $# -lt 1 ]; then
-  echo "usage: $0 [bitcoin srcroot] build-test arguments..."
-fi
-
-cd $1
-shift
-
-./autogen.sh
-./configure
-./qa/pull-tester/build-tests.sh "$@"
+// No build information available
+#define BUILD_DATE "2015-12-10 16:57:19 -0800"
